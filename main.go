26d98dc07b8142d636e02a57697e9d0a82750dce
kcastel <keviniicastel@gmail.com>
2023-11-08 21:17:14 +0100

    main.go->handler

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
-	Reussite "reussite/utils"
+	reussite "reussite/utils"
 )
 
 const PORT = "8020"
 
 func main() {
 	fmt.Println("Access to the web server by this one: http://localhost:" + PORT + "/home")
+	fileServerCss := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServerCss))
 
-	http.HandleFunc("/home", Reussite.mainPage)
+	http.HandleFunc("/home", reussite.MainPageHandler)
 	http.ListenAndServe("localhost:"+PORT, nil)
 }
