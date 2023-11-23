78ef1c7b86171fe871d6f364a94f38f7396170c4
kcastel <keviniicastel@gmail.com>
2023-11-23 12:20:44 +0100

    implémentation du web scrapping

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 
 const PORT = "8020"
 
+func init() {
+	reussite.GetExercices()
+}
+
 func main() {
 	fmt.Println("Access to the web server by this one: http://localhost:" + PORT + "/home")
 	fileServerCss := http.FileServer(http.Dir("static"))
@@ -16,3 +20,16 @@ func main() {
 	http.HandleFunc("/home", reussite.MainPageHandler)
 	http.ListenAndServe("localhost:"+PORT, nil)
 }
+
+/*
+   exercises := map[int][]string{
+       1: []string{"displayfirstparam", "displaylastparam", "displayz", "displaya", "hello", "onlya", "onlyz", "printdigits", "strlen", "paramcount", "displayalpham", "displayalrevm", "countdown"},
+       2: []string{"nrune", "lastrune", "printstr", "strrev", "firstrune", "printreversealphabet", "printalphabet", "wdmatch", "ispowerof2", "rot13", "lastword", "rot14", "max", "reduceint"},
+       3: []string{"switchcase", "swap", "compare", "expandstr", "tabmult", "searchreplace", "alphamirror", "doop", "findprevprime", "reversebits", "chunk", "foldint"},
+       4: []string{"swapbits", "capitalize", "repeatalpha", "atoi", "reversestrcap", "printbits", "inter", "union", "piglatin", "romannumbers"},
+       5: []string{"firstword", "sortwordarr", "cleanstr", "addprimesum", "printhex", "gcd"},
+       6: []string{"printcomb", "split", "hiddenp", "rostring", "revwstr", "reverserange", "range", "slice"},
+       7: []string{"itoa", "atoibase", "foreach", "fprime", "printrevcomb"},
+       8: []string{"listsize", "rpncalc", "brackets", "options", "grouping", "printmemory"},
+       9: []string{"listremoveif", "itoabase", "brainfuck"},
+   }*/
