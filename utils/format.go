e09ca0c854cf895bc8f19b4311bd752584a9e24b
kcastel <keviniicastel@gmail.com>
2023-11-25 17:46:45 +0100

    Implémentation de la fonctionnalité pour le formattage de texte ainsi pour la manipulation des données des exercices

--- utils/format.go
+++ utils/format.go
@@ -7,7 +7,8 @@ import (
 
 // Called for formatting each string elements of an array,
 // The array passed as argument have to contains "rules" and "output"
-func FormatExercices(a []string) {
+// So return-it as an array of string
+func FormatExercices(exercicesContents StringSlice, link string) []string {
 	var typeLine string
 	var actualType string
 
@@ -16,25 +17,53 @@ func FormatExercices(a []string) {
 		"(.*)(Usage)(.*)":       "usage",
 	}
 
-	for _, line := range a {
+	mapResultArray := make(map[string][]string, 0)
+
+	for _, line := range exercicesContents {
 		for pattern, patternType := range mapPattern {
 			actualType = GetLineType(line, pattern, patternType)
 			if actualType != "" {
 				typeLine = actualType
 			}
 		}
+		if typeLine == "instruction" || typeLine == "usage" {
+			mapResultArray[typeLine] = append(mapResultArray[typeLine], line)
+		}
+	}
 
-		if typeLine == "instruction" {
+	exercicesContents.Clear()
 
-		} else if typeLine == "usage" {
+	exercicesContents = append(exercicesContents, GetExerciceName(link))
 
+	var result string
+	var lastType string
+
+	for currentType, arrayContent := range mapResultArray {
+		for indexLine, line := range arrayContent {
+			if indexLine > 0 && IsStringMatched(line, "(.*)(Instructions|Usage)(.*)") {
+				result += line + "\n"
+			}
 		}
 
+		if lastType != currentType {
+			lastType = currentType
+			exercicesContents = append(exercicesContents, result)
+			result = ""
+		}
 	}
-	fmt.Println(a)
+
+	return exercicesContents
 }
 
-// Called for regex
+// Called for getting if the string matched or not
+func IsStringMatched(s, p string) bool {
+	regexpObj, _ := regexp.Compile("")
+	match := regexpObj.FindStringSubmatch(s)
+
+	return (len(match) > 0)
+}
+
+// Called for getting an line type(using regex)
 func GetLineType(s, pattern, typeLine string) string {
 	regexObj, _ := regexp.Compile(pattern)
 	match := regexObj.FindStringSubmatch(s)
@@ -44,4 +73,19 @@ func GetLineType(s, pattern, typeLine string) string {
 		return ""
 	}
 
+}
+
+// Called for getting an exercice name from an link by using the regex tools
+func GetExerciceName(link string) string {
+	var pattern string = "(https://github.com/01-edu/public/blob/master/subjects/)(?P<exerciceName>.*)((/README.md?plain=1)*)"
+	fmt.Println("link:", link)
+	regexObj, _ := regexp.Compile(pattern)
+	match := regexObj.FindStringSubmatch(link)
+	if len(match) > 0 {
+
+		matchIndex := regexObj.SubexpIndex("exerciceName")
+		return match[matchIndex]
+	}
+
+	return ""
 }
